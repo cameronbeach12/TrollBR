559fa34b7e8e736b904d6bc01837b0c02cbe8e6b
cameronbeach12 <37076551+cameronbeach12@users.noreply.github.com>
2018-03-14 10:01:34 -0400

    Add files via upload
    CURRENTLY CRASHING!

---
+++ scripts/KeyBindManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyBindManager : MonoBehaviour {
+
+	const string MOVE_FORWARD_KEY = "move_forward";
+
+	// Use this for initialization
+	public IEnumerator SetMoveForward(bool keyPressed){
+			while (!Input.anyKeyDown && keyPressed == false) {
+				if (Input.anyKeyDown) {
+					foreach (KeyCode vKey in System.Enum.GetValues(typeof(KeyCode))) {
+						if (Input.GetKey (vKey)) {
+							string nKey = vKey.ToString ();
+							PlayerPrefs.SetString (MOVE_FORWARD_KEY, nKey);
+							keyPressed = true;
+							yield break;
+					}
+				} 
+
+			}
+		}
+	}
+
+	public void SetMF(){
+		StartCoroutine (SetMoveForward (false));
+	}
+}
